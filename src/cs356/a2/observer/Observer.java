4a63eb14095b629d171cbead3a36ca35d385b1ef
Jenifer Wang <jeniferwang@csupomona.edu>
2014-11-16 00:05:46 -0800

    Removing Observer and Subject files since using Observable and Observer Java library

--- src/cs356/a2/observer/Observer.java
+++
@@ -1,7 +0,0 @@
-package cs356.a2.observer;
-
-public interface Observer {
-	
-	public void update();
-
-}
--- src/cs356/a2/observer/Subject.java
+++
@@ -1,9 +0,0 @@
-package cs356.a2.observer;
-
-public interface Subject {
-	
-	public void addObserver(Observer observer);
-	public void removeObserver(Observer observer);
-	public void notify(Object object);
-
-}
